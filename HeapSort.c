6d5e9bb397094e20e7e57d8438fa3daa3808042f
Phantom <56475942+Primer-Line@users.noreply.github.com>
2019-12-27 21:44:08 +0800

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-//¶ÑÅÅĞò
+//å †æ’åº
 void swap(int* a,int* b)
 {
 	int temp = *a;
@@ -13,16 +13,13 @@ void adjustHeap(int* arr,int n)
 	int i,temp;
 	for(i = n/2-1;i>=0;i--)
 	{
-		if(i == n/2 - 1 && 2*i < n - 3)temp = arr[2*i+1];
-		else temp = arr[2*i+1] > arr[2*i+2] ? arr[2*i+1]:arr[2*i+2];
+		temp = i == n/2 - 1 && 2*i < n - 3 ? arr[2*i+1] : (arr[2*i+1] > arr[2*i+2] ? arr[2*i+1]:arr[2*i+2]);
  		if(arr[i]<temp)swap(&arr[i],temp == arr[2*i+1]?&arr[2*i+1]:&arr[2*i+2]);
 	}
 	for(i = 1;i<=n/2-1;i++)
 	{
-		
-		if(i == n/2 - 1 && 2*i < n - 3)temp = arr[2*i+1];
-		else temp = arr[2*i+1] > arr[2*i+2] ? arr[2*i+1]:arr[2*i+2];
-		if(arr[i]<temp)swap(&arr[i],temp == arr[2*i+1]?&arr[2*i+1]:&arr[2*i+2]);
+		temp = i == n/2 - 1 && 2*i < n - 3 ? arr[2*i+1] : (arr[2*i+1] > arr[2*i+2] ? arr[2*i+1]:arr[2*i+2]);
+ 		if(arr[i]<temp)swap(&arr[i],temp == arr[2*i+1]?&arr[2*i+1]:&arr[2*i+2]);
 	}
 	
 }
@@ -38,18 +35,12 @@ int main()
 {
 	int arr[]={5,4,11,8,9,6,12,8,0,10},* sortArr;
 	int n = sizeof(arr)/sizeof(int),i=0;
-	printf("Ô­Êı×é:\n");
-	for(;i<n;i++)
-	{
-		printf("%d ",arr[i]);
-	}
+	printf("åŸæ•°ç»„:\n");
+	for(;i<n;i++)printf("%d ",arr[i]);
 	printf("\n");
 	heapSort(arr,n);
-	printf("¶ÑÅÅĞòÊı×é:\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d ",arr[i]);
-	}
+	printf("å †æ’åºæ•°ç»„:\n");
+	for(i=0;i<n;i++)printf("%d ",arr[i]);
 	printf("\n");
 	return 0;
 }
